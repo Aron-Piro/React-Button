a053abd89cd907874d2dc3bd4487e62755173da0
Aron Pironato <aron.pironato58@gmail.com>
2023-04-11 22:07:13 -0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <>
+      <h1>Lista de presença</h1>
+      <input type="type" placeholder="Digite o nome..." />
+      <button type="button">Adicionar</button>
+    </>
+  )
+}
+
+export default Home;
